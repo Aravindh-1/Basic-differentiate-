4088f0e18198e8b3c54574d1ff7b465a571f4797
Aravindh M A <120136311+Aravindh-1@users.noreply.github.com>
2024-06-18 12:30:02 +0530

    done

---
+++ Differentiate.py
@@ -0,0 +1,14 @@
+class baby:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def differentiate(a,b):
+        c=(a**2)+(b**2)
+        diff_c= 2*a + 2*b
+        if user=="multiply":
+            return c
+        else:
+            return diff_c
+user=input()
+d=baby.differentiate(11,20)
+print(d)
